20e9d1758d0b7063623f7ecdf48dbf764085fb92
yangsungsim <yangsungsim227@gmail.com>
2022-07-01 17:49:05 +0900

    initial commit

---
+++ src/main/java/com/example/lazyaddress/LazyAddressApplication.java
@@ -0,0 +1,13 @@
+package com.example.lazyaddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyAddressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LazyAddressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lazyaddress/LazyAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lazyaddress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyAddressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
